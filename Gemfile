0306da34bda3344f552b5d51a40f5ac202260858
Ruan Nawe <ruannawe95@gmail.com>
2023-11-17 17:12:05 -0300

    add fake users and addresses

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 
 gem "byebug"
+gem 'faker', '~> 3.2', '>= 3.2.2'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+100.times do |i|
+  citizen = Citizen.create(
+    full_name: Faker::Name.name,
+    tax_id: Faker::IDNumber.brazilian_citizen_number,
+    national_health_card: Faker::Number.number(digits: 15),
+    email: Faker::Internet.email,
+    birthdate: Faker::Date.between(from: '1950-01-01', to: '2000-12-31'),
+    phone: Faker::PhoneNumber.phone_number,
+    photo: Faker::Avatar.image(size: "100x100", format: 'jpg'),
+    status: [true, false].sample
+  )
+
+  address = Address.create(
+    citizen_id: citizen.id,
+    zip_code: Faker::Address.postcode,
+    street: Faker::Address.street_address,
+    complement: Faker::Address.secondary_address,
+    neighborhood: Faker::Address.community,
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    ibge_code: Faker::Number.number(digits: 7)
+  )
+end
