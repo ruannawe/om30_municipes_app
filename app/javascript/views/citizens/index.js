5d81351df445e76bd1cbeb64c3c55b57cad4ab91
Ruan Nawe <ruannawe95@gmail.com>
2023-11-18 16:00:07 -0300

    refactoring js file responsible for manage filters

--- app/javascript/views/citizens/index.js
+++ app/javascript/views/citizens/index.js
@@ -1,16 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('filter-form').addEventListener('submit', function (event) {
-        event.preventDefault()
+document.addEventListener('DOMContentLoaded', () => {
+    const filterForm = document.getElementById('filter-form')
 
-        const formData = new FormData(this)
-        const searchParams = new URLSearchParams()
-
-        for (const [key, value] of formData) {
-            if (value.length > 0) {
-                searchParams.append(key, value)
-            }
-        }
-
-        window.location = this.action + '?' + searchParams.toString()
-    })
+    if (filterForm) {
+        filterForm.addEventListener('submit', handleFormSubmit)
+    }
 })
+
+const handleFormSubmit = event => {
+    event.preventDefault()
+    const formElement = event.currentTarget
+    const formData = new FormData(formElement)
+    const queryString = buildQueryString(formData)
+
+    redirectToFilteredUrl(formElement.action, queryString)
+}
+
+const buildQueryString = formData => {
+    const searchParams = new URLSearchParams()
+
+    for (const [key, value] of formData) {
+        if (value.length > 0) {
+            searchParams.append(key, value)
+        }
+    }
+    return searchParams.toString()
+}
+
+const redirectToFilteredUrl = (action, queryString) => {
+    window.location = action + '?' + queryString
+}
