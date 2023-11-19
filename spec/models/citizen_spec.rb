efbc8d3db8eca45fda8f7931bebba4b4ea8a8436
Ruan Nawe <ruannawe95@gmail.com>
2023-11-19 14:55:25 -0300

    replace model objects by factories in model tests

--- spec/models/citizen_spec.rb
+++ spec/models/citizen_spec.rb
@@ -2,158 +2,118 @@ require 'rails_helper'
 
 RSpec.describe Citizen, type: :model do
   describe 'validations' do
-    before do
-      @citizen = Citizen.new(
-        full_name: 'John Doe',
-        tax_id: '93343339016',
-        national_health_card: '239233319950009',
-        email: 'johndoe@example.com',
-        birthdate: Date.new(1980, 1, 1),
-        phone: '1234567890',
-        status: true
-      )
-    end
+    let(:citizen) { build(:citizen) }
 
     it 'is valid with all attributes' do
-      expect(@citizen).to be_valid
+      expect(citizen).to be_valid
     end
 
     it 'is invalid without a full_name' do
-      @citizen.full_name = nil
-      expect(@citizen).to_not be_valid
-      expect(@citizen.errors[:full_name]).to include("can't be blank")
+      citizen.full_name = nil
+      expect(citizen).to_not be_valid
+      expect(citizen.errors[:full_name]).to include("can't be blank")
     end
 
     it 'is invalid without a tax_id' do
-      @citizen.tax_id = nil
-      expect(@citizen).to_not be_valid
-      expect(@citizen.errors[:tax_id]).to include("can't be blank")
+      citizen.tax_id = nil
+      expect(citizen).to_not be_valid
+      expect(citizen.errors[:tax_id]).to include("can't be blank")
     end
 
     it 'is invalid without a national_health_card' do
-      @citizen.national_health_card = nil
-      expect(@citizen).to_not be_valid
-      expect(@citizen.errors[:national_health_card]).to include("can't be blank")
+      citizen.national_health_card = nil
+      expect(citizen).to_not be_valid
+      expect(citizen.errors[:national_health_card]).to include("can't be blank")
     end
 
     it 'is invalid without an email' do
-      @citizen.email = nil
-      expect(@citizen).to_not be_valid
-      expect(@citizen.errors[:email]).to include("can't be blank")
+      citizen.email = nil
+      expect(citizen).to_not be_valid
+      expect(citizen.errors[:email]).to include("can't be blank")
     end
 
     it 'is invalid without a birthdate' do
-      @citizen.birthdate = nil
-      expect(@citizen).to_not be_valid
-      expect(@citizen.errors[:birthdate]).to include("can't be blank")
+      citizen.birthdate = nil
+      expect(citizen).to_not be_valid
+      expect(citizen.errors[:birthdate]).to include("can't be blank")
     end
 
     it 'is invalid without a phone' do
-      @citizen.phone = nil
-      expect(@citizen).to_not be_valid
-      expect(@citizen.errors[:phone]).to include("can't be blank")
+      citizen.phone = nil
+      expect(citizen).to_not be_valid
+      expect(citizen.errors[:phone]).to include("can't be blank")
     end
 
     it 'is valid with a false status' do
-      @citizen.status = false
-      expect(@citizen).to be_valid
+      citizen.status = false
+      expect(citizen).to be_valid
     end
 
     it 'is valid with a true status' do
-      @citizen.status = true
-      expect(@citizen).to be_valid
+      citizen.status = true
+      expect(citizen).to be_valid
     end
   end
 
   describe 'scopes' do
-    before do
-      @citizen1 = Citizen.create!(
-        full_name: 'Alice Smith',
-        tax_id: '51017822115',
-        national_health_card: '239233319950009',
-        email: 'alice@example.com',
-        birthdate: Date.new(1990, 1, 1),
-        phone: '1234567890',
-        status: true
-      )
-      @citizen2 = Citizen.create!(
-        full_name: 'Bob Jones',
-        tax_id: '39655368262',
-        national_health_card: '954440753770000',
-        email: 'bob@example.com',
-        birthdate: Date.new(1995, 1, 1),
-        phone: '0987654321',
-        status: false
-      )
-    end
+    let(:citizen1) { create(:citizen, full_name: 'Alice Smith', tax_id: '51017822115', national_health_card: '239233319950009', email: 'alice@example.com', birthdate: Date.new(1990, 1, 1), phone: '1234567890', status: true) }
+    let(:citizen2) { create(:citizen, full_name: 'Bob Jones', tax_id: '39655368262', national_health_card: '954440753770000', email: 'bob@example.com', birthdate: Date.new(1995, 1, 1), phone: '0987654321', status: false) }
 
     it 'filters by full_name' do
-      expect(Citizen.filter_by_full_name('Alice')).to include(@citizen1)
-      expect(Citizen.filter_by_full_name('Alice')).to_not include(@citizen2)
+      expect(Citizen.filter_by_full_name('Alice')).to include(citizen1)
+      expect(Citizen.filter_by_full_name('Alice')).to_not include(citizen2)
     end
 
     it 'filters by tax_id' do
-      expect(Citizen.filter_by_tax_id('51017822115')).to include(@citizen1)
-      expect(Citizen.filter_by_tax_id('51017822115')).to_not include(@citizen2)
+      expect(Citizen.filter_by_tax_id('51017822115')).to include(citizen1)
+      expect(Citizen.filter_by_tax_id('51017822115')).to_not include(citizen2)
     end
 
     it 'filters by national_health_card' do
-      expect(Citizen.filter_by_national_health_card('239233319950009')).to include(@citizen1)
-      expect(Citizen.filter_by_national_health_card('239233319950009')).to_not include(@citizen2)
+      expect(Citizen.filter_by_national_health_card('239233319950009')).to include(citizen1)
+      expect(Citizen.filter_by_national_health_card('239233319950009')).to_not include(citizen2)
     end
 
     it 'filters by email' do
-      expect(Citizen.filter_by_email('alice@example.com')).to include(@citizen1)
-      expect(Citizen.filter_by_email('alice@example.com')).to_not include(@citizen2)
+      expect(Citizen.filter_by_email('alice@example.com')).to include(citizen1)
+      expect(Citizen.filter_by_email('alice@example.com')).to_not include(citizen2)
     end
 
     it 'filters by birthdate' do
-      expect(Citizen.filter_by_birthdate(Date.new(1990, 1, 1))).to include(@citizen1)
-      expect(Citizen.filter_by_birthdate(Date.new(1990, 1, 1))).to_not include(@citizen2)
+      expect(Citizen.filter_by_birthdate(Date.new(1990, 1, 1))).to include(citizen1)
+      expect(Citizen.filter_by_birthdate(Date.new(1990, 1, 1))).to_not include(citizen2)
     end
 
     it 'filters by phone' do
-      expect(Citizen.filter_by_phone('1234567890')).to include(@citizen1)
-      expect(Citizen.filter_by_phone('1234567890')).to_not include(@citizen2)
+      expect(Citizen.filter_by_phone('1234567890')).to include(citizen1)
+      expect(Citizen.filter_by_phone('1234567890')).to_not include(citizen2)
     end
   end
 
   describe 'birthdate validations' do
-    let(:valid_attributes) do
-      {
-        full_name: 'John Doe',
-        tax_id: '43484923369',
-        national_health_card: '239233319950009',
-        email: 'john@example.com',
-        phone: '1234567890'
-      }
+    let(:valid_attributes) { attributes_for(:citizen).except(:birthdate) }
+
+    it 'is valid with a recent birthdate' do
+      citizen = Citizen.new(valid_attributes.merge(birthdate: Date.today - 30.years))
+      expect(citizen).to be_valid
     end
 
-    context 'when birthdate is valid' do
-      it 'is valid with a recent birthdate' do
-        citizen = Citizen.new(valid_attributes.merge(birthdate: Date.today - 30.years))
-        expect(citizen).to be_valid
-      end
+    it 'is invalid with a future birthdate' do
+      citizen = Citizen.new(valid_attributes.merge(birthdate: Date.tomorrow))
+      citizen.valid?
+      expect(citizen.errors[:birthdate]).to include("can't be in the future")
     end
 
-    context 'when birthdate is invalid' do
-      it 'is invalid with a future birthdate' do
-        citizen = Citizen.new(valid_attributes.merge(birthdate: Date.tomorrow))
-        citizen.valid?
-        expect(citizen.errors[:birthdate]).to include("can't be in the future")
-      end
+    it 'is invalid with a birthdate more than 150 years ago' do
+      citizen = Citizen.new(valid_attributes.merge(birthdate: 151.years.ago))
+      citizen.valid?
+      expect(citizen.errors[:birthdate]).to include("can't be more than 150 years ago")
+    end
 
-      it 'is invalid with a birthdate more than 150 years ago' do
-        citizen = Citizen.new(valid_attributes.merge(birthdate: 151.years.ago))
-        citizen.valid?
-        expect(citizen.errors[:birthdate]).to include("can't be more than 150 years ago")
-      end
-
-      it 'is invalid with a non-date birthdate' do
-        citizen = Citizen.new(valid_attributes.merge(birthdate: 'not-a-date'))
-        citizen.valid?
-        expect(citizen.errors[:birthdate]).to include("can't be blank")
-      end
+    it 'is invalid with a non-date birthdate' do
+      citizen = Citizen.new(valid_attributes.merge(birthdate: 'not-a-date'))
+      citizen.valid?
+      expect(citizen.errors[:birthdate]).to include("can't be blank")
     end
   end
 
@@ -181,20 +141,17 @@ RSpec.describe Citizen, type: :model do
   describe 'CNS validations' do
     it 'is valid with a valid CNS number' do
       citizen = create(:citizen, national_health_card: '239233319950009')
-
       expect(citizen).to be_valid
     end
 
     it 'is invalid with an invalid CNS number' do
       citizen = build(:citizen, national_health_card: '239233310000000')
-
       citizen.valid?
       expect(citizen.errors[:national_health_card]).to include('is not a valid CNS number')
     end
 
     it 'is invalid with a CNS number of incorrect length' do
       citizen = build(:citizen, national_health_card: 'short')
-
       citizen.valid?
       expect(citizen.errors[:national_health_card]).to include('is not a valid CNS number')
     end
