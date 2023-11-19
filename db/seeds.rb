240c007dffb422e57a431c377dd331fe93e7c01a
Ruan Nawe <ruannawe95@gmail.com>
2023-11-19 19:57:40 -0300

    fixing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ def generate_cns(definitive: true)
   loop do
     prefix = definitive ? rand(1..2).to_s : rand(7..9).to_s
     cns_number = prefix + '%014d' % rand(10**14)
-    return cns_number if valid_cns_number?(cns_number)
+    return cns_number
   end
 end
 
@@ -20,7 +20,7 @@ end
   citizen = Citizen.create(
     full_name: Faker::Name.name,
     tax_id: Faker::IDNumber.brazilian_citizen_number,
-    national_health_card: generate_cns([true, false].sample),
+    national_health_card: generate_cns(definitive: [true, false].sample),
     email: Faker::Internet.email,
     birthdate: Faker::Date.between(from: '1950-01-01', to: '2000-12-31'),
     phone: Faker::PhoneNumber.phone_number,
