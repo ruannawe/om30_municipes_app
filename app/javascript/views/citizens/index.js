181665e10fd16f67e37099ffc0b20f593c1b46ae
Ruan Nawe <ruannawe95@gmail.com>
2023-11-21 19:06:10 -0300

    complement other address fields based on zip code

--- app/javascript/views/citizens/index.js
+++ app/javascript/views/citizens/index.js
@@ -8,6 +8,28 @@ document.addEventListener('DOMContentLoaded', () => {
     if (resetButton) {
         resetButton.addEventListener('click', clearQueryParamsAndReload)
     }
+
+    const zipCodeField = document.getElementById('address_zip_code')
+    zipCodeField.addEventListener('change', () => {
+        const zipCode = zipCodeField.value
+        console.log('hello')
+        console.log(zipCode)
+
+        if (zipCode.length === 8) {
+            fetch(`https://app.zipcodebase.com/api/v1/search?codes=${zipCode}&apikey=63750c50-88b2-11ee-a503-87b46622915b`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(JSON.stringify(data))
+                    const addressInfo = data.results[zipCode][0]
+                    document.getElementById('address_street').value = addressInfo.province // 'province' seems to be the street info
+                    document.getElementById('address_neighborhood').value = addressInfo.city // 'city' is being used for neighborhood
+                    document.getElementById('address_city').value = addressInfo.city_en // City in English
+                    document.getElementById('address_state').value = addressInfo.state_en // State in English
+                    document.getElementById('address_ibge_code').value = addressInfo.province_code // IBGE code
+                })
+                .catch(error => console.error('Error:', error))
+        }
+    })
 })
 
 const handleFormSubmit = event => {
